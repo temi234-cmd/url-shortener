604f4bb48337d062536c3b72d4f96c93b5c879f0
temi <ogtm1011@gmail.com>
2026-04-26 21:03:41 +0100

    Fix CORS for production

--- server/index.js
+++ server/index.js
@@ -5,7 +5,11 @@ require('dotenv').config();
 const Url = require('./models/Url');
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 // Routes (we'll add these soon)
